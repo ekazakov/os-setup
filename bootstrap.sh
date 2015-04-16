5dd25bb2a39f70220a49ec33e8f005972a2a83ff
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-04-17 00:47:15 +0300

    Init git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
+
+git config --global user.email "evgeniy.kazakov@gmail.com"
+git config --global user.name "Evgeniy Kazakov"
+
 sudo add-apt-repository ppa:rquillo/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
