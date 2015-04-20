215af53b63ad432a711ac48b9978e50f1accc21d
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-04-21 00:09:04 +0300

    Update paths to home dir and zsh tasks

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ git config --global user.email "evgeniy.kazakov@gmail.com"
 git config --global user.name "Evgeniy Kazakov"
 
 sudo add-apt-repository ppa:rquillo/ansible -y
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
 sudo ansible-playbook -K -i inventory playbook.yml --extra-vars "user=evgeniy"
