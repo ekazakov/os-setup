39adeaf16abf9479809169cc21e66218acd1351a
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-04-27 16:29:10 +0300

    Fixes and minor tasks update

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 sudo add-apt-repository ppa:rquillo/ansible -y
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update -y
 sudo apt-get install ansible -y
