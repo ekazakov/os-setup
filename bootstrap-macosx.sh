5c9cd036366582ca75bc4a0e613e4ffef030a36b
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2019-10-28 21:32:29 +0100

    update

--- bootstrap-macosx.sh
+++ bootstrap-macosx.sh
@@ -1,6 +1,51 @@
-# !/bin/bash
+#!/bin/bash
+
+function error_message() {
+  echo -e "Script failed on line $1 of the deployment script"
+}
+
+trap 'error_message $LINENO' ERR
+
+set -o pipefail
+set -e
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
+brew tap caskroom/cask
 brew install caskroom/cask/brew-cask
 brew install ansible
+
+TEMPLATE="
+Host localhost
+  ForwardAgent yes
+
+Host *
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/github_rsa
+  IdentityFile ~/.ssh/bitbucket_rsa
+"
+
+DESTINATION="${HOME}/.ssh/config"
+
+printf "\n******** Generate github access key\n"
+ssh-keygen -N "" -t rsa -b 4096 -f ~/.ssh/github_rsa -C "github.com key by evgeniy.kazakov@gmail.com"
+
+printf "\n******** Generate bitbucket access key\n"
+ssh-keygen -N "" -t rsa -b 4096 -f ~/.ssh/bitbucket_rsa -C "bitbucket.org key by evgeniy.kazakov@gmail.com"
+
+printf "\n******** Run ssh-agent\n"
+eval "$(ssh-agent -s)"
+
+printf "\n******** Add keys to ssh-agent\n"
+ssh-add -K ~/.ssh/github_rsa
+ssh-add -K ~/.ssh/bitbucket_rsa
+
+printf "\n******** Create user ssh config\n"
+
+mkdir -p "${HOME}/.ssh"
+if [ -f "${DESTINATION}" ]; then
+	mv "${DESTINATION}" "${DESTINATION}_old"
+fi
+
+echo "${TEMPLATE}" > "${DESTINATION}"
