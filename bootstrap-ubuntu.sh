f58154bdeacc6dc7a9ffaa84822297eb2b2e5d75
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2019-11-02 22:00:26 +0100

    Update

--- bootstrap-ubuntu.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-sudo add-apt-repository ppa:rquillo/ansible -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo apt-get update -y
-sudo apt-get install ansible -y
