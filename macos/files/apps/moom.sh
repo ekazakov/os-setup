e567b3a90b936879f535b3ad285c8fbb2a73bf64
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2019-11-10 21:53:47 +0100

    update

--- macos/files/apps/moom.sh
+++ macos/files/apps/moom.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #########################################################################################################
 # Moom                                                                                                  #
 #########################################################################################################
 
-killall Moom
-killall cfprefsd #disable prefence daemon to reset plist cache
+killall Moom &> /dev/null
+killall cfprefsd &> /dev/null #disable prefence daemon to reset plist cache
 
 MOOM_CONFIG="com.manytricks.moom.plist"
-plutil -convert binary1 -o "${HOME}/Library/Preferences/${MOOM_CONFIG}" "${MOOM_CONFIG}.json"
+plutil -convert binary1 -o "${HOME}/Library/Preferences/${MOOM_CONFIG}" "${HOME}/dotfiles/${MOOM_CONFIG}.json"
