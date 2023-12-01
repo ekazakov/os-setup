58709b9ed6ab31d01124d9518bdd342ae5035a9b
Evgenii Kazakov <evgeniy.kazakov@gmail.com>
2023-12-01 16:19:09 +0100

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #git config --global user.email "evgeniy.kazakov@gmail.com"
 #git config --global user.name "Evgeniy Kazakov"
 
-ansible-playbook playbook.yml "${@}"
+ansible-playbook -i hosts playbook.yml "${@}"
 #--extra-vars "$1" --tags "$2"
