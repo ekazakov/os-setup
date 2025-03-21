0c9bbb3a55dd135630588681d96c399e1213fd43
Evgenii Kazakov- <evgeniy.kazakov@gmail.com>
2025-03-21 18:55:05 +0100

    update brew install and add ghostty config

--- bootstrap-macosx.sh
+++ bootstrap-macosx.sh
@@ -10,6 +10,9 @@ set -o pipefail
 set -e
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> "${HOME}/.zprofile"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "${HOME}/.zprofile"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew doctor
 brew install ansible
 
