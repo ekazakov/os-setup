6daeceff289a14a7e3406e78c83accd697819c39
Evgenii Kazakov <evgeniy.kazakov@gmail.com>
2025-03-21 22:07:37 +0100

    update

---
+++ 01-setup-dofiles.sh
@@ -0,0 +1,1 @@
+ansible-playbook -K -i inventory playbook.yml --tags dotfiles
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #git config --global user.email "evgeniy.kazakov@gmail.com"
-#git config --global user.name "Evgeniy Kazakov"
+#git config --global user.name "Evgenii Kazakov"
 
 ansible-playbook -i hosts playbook.yml "${@}"
 #--extra-vars "$1" --tags "$2"
